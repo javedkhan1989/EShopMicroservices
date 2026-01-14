835c0d88bd5de20f832c3efc4c75a1d505d8c078
javedkhan1989 <jksoft2010@gmail.com>
2026-01-14 13:29:37 +0530

    update data on 14th Jan

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
+    public record CreateProductResult(Guid Id);
+    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    {
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            //Bussiness Logic to create a product
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -0,0 +1,8 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Add services to the container
+var app = builder.Build();
+
+//Configure the Http Request Pipeline
+
+app.Run();
