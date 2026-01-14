a42893063179e44259a5488180d6dda79dbc71d0
javedkhan1989 <jksoft2010@gmail.com>
2026-01-14 23:13:55 +0530

    update 14 Jan

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,16 +1,32 @@
-﻿using MediatR;
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+using MediatR;
 
 namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string description, string ImageFile, decimal Price)
-        : IRequest<CreateProductResult>;
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : ICommand<CreateProductResult>;
     public record CreateProductResult(Guid Id);
-    internal class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
+    internal class CreateProductCommandHandler 
+        : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
-        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
-            //Bussiness Logic to create a product
-            throw new NotImplementedException();
+            //create Product entity from command object
+            var Product = new Product
+            {
+                Name= command.Name,
+                Category= command.Category,
+                Description=command.Description,
+                ImageFile=command.ImageFile,
+                Price= command.Price
+
+            };
+            //TODO
+            //save to database
+            //return result
+
+            return new  CreateProductResult(Guid.NewGuid());
         }
     }
 }
--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,8 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
 var app = builder.Build();
 
 //Configure the Http Request Pipeline
-
+app.MapCarter();
 app.Run();
