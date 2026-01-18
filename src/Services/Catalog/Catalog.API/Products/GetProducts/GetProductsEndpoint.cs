735f9a09ebe26dabda062f0ca094f1d34e0ac1d6
javedkhan1989 <jksoft2010@gmail.com>
2026-01-18 18:40:55 +0530

    update 18th Jan

---
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Products.CreateProduct;
+
+namespace Catalog.API.Products.GetProducts
+{
+    public record GetProductsRequest();
+
+    public record GetProductsResponse(IEnumerable<Product> Products);
+    public class GetProductsEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products",async (ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsQuery());
+
+                var respone = result.Adapt<GetProductsResponse>();
+
+                return Results.Ok(respone);
+            })
+                .WithName("GetProducts")
+            .Produces<GetProductsResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products")
+            .WithDescription("Get Produts");
+        }
+    }
+}
