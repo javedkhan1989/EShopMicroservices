a643f10ff2dc2e774f8712e7d6265ac814cae4f4
javedkhan1989 <jksoft2010@gmail.com>
2026-01-19 17:43:33 +0530

    Update 19th Jan

---
+++ src/Services/Catalog/Catalog.API/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -0,0 +1,28 @@
+﻿
+using Catalog.API.Products.GetProductByCategory;
+
+namespace Catalog.API.Products.DeleteProduct
+{
+
+    //public record DeleteProductRequest(Guid ProductId);
+
+    public record DeleteProductResponse(bool Success);
+    public class DeleteProductEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+
+                var result = await sender.Send(new DeleteProductCommand(id));
+                var response= result.Adapt<DeleteProductResponse>();
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<DeleteProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
+    }
+}
